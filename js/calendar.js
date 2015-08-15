3c0cd65532ade7e7c965b5716d7fa26f1464a9bb
Rafael Morales <rafael.jose1201@gmail.com>
2015-08-15 11:53:30 -0430

    Fixed collisions of the new events with the existing ones, asking the user to select new times, starting to work into avoid selection of past days and hours.

--- js/calendar.js
+++ js/calendar.js
@@ -5,6 +5,7 @@
         var $calendar = $('#calendar');
         var lastView;
         var i = 0;
+        var create = true;
 
         // Init calendar
         $calendar.fullCalendar({
@@ -17,12 +18,15 @@
             selectHelper: true,
             // Triger function when event clicked
             select: function(start, end) {
+                // Get the current view of the calendar
                 var view = ($calendar.fullCalendar('getView'));
+                // if view is agendaDay evaluate if counter of selects inside the calendar is > than 1, as the calendar as soon as you click a day switch to agenda day and we are trying to get the time selection event and not navigation one
                 if(view.name == 'agendaDay'){
                     i++;
                     if(i > 1){
                         var title = prompt('Event Title:');
                         var eventData;
+                        // Store the new event data
                         if (title) {
                             eventData = {
                                 title: title,
@@ -36,21 +40,41 @@
                                 var existingEventDateEnd = existingEvents.end._d.toJSON().split('T')[0];
                                 var existingEventTimeStart = existingEvents.start._d.toJSON().split('T')[1].split(':')[0];
                                 var existingEventTimeEnd = existingEvents.end._d.toJSON().split('T')[1].split(':')[0];
+                                var existingEventDuration = parseInt(existingEventTimeEnd) - parseInt(existingEventTimeStart);
 
                                 // New event Data
                                 var newEventDateStart = eventData.start._d.toJSON().split('T')[0];
                                 var newEventDateEnd = eventData.end._d.toJSON().split('T')[0];
                                 var newEventTimeStart = eventData.start._d.toJSON().split('T')[1].split(':')[0];
                                 var newEventTimeEnd = eventData.end._d.toJSON().split('T')[1].split(':')[0];
+                                var newEventDuration = parseInt(newEventTimeEnd) - parseInt(newEventTimeStart);
 
                                 if(existingEventDateStart == newEventDateStart){
-                                    if(parseInt(newEventTimeStart) <= parseInt(existingEventTimeStart) && parseInt(newEventTimeEnd) <= parseInt(existingEventTimeEnd) && parseInt(newEventTimeEnd) > parseInt(existingEventTimeStart)){
-                                        console.log('They Colapse');
+                                    
+                                    // calculate the diference betwen the time start of the existing and the new event
+                                    var collapseCalcForEarlyNew = parseInt(existingEventTimeStart) - parseInt(newEventTimeStart);
+                                    var collapseCalcForLaterNew = parseInt(newEventTimeStart) - parseInt(existingEventTimeStart);
+
+                                    // if the start of the new event is earlier than the existing one evaluate the diference which if less than diference is a collapse, then if the start of the new event is later than the existing one evaluate the diference which if les than the existing event duration is a collapse, then evaluate if events start and end at same time which is obviusly a collapse =D, if there is a collapse var create turns into false.                                   
+                                    if( ( parseInt(newEventTimeStart) < parseInt(existingEventTimeStart) && parseInt(newEventDuration) > parseInt(collapseCalcForEarlyNew) ) || ( parseInt(newEventTimeStart) > parseInt(existingEventTimeStart) && parseInt(existingEventDuration) > parseInt(collapseCalcForLaterNew) ) || ( parseInt(newEventTimeStart) === parseInt(existingEventTimeStart) && parseInt(newEventTimeEnd) === parseInt(existingEventTimeEnd) ) ){
+
+                                        create = false;
+
                                     }
                                 }
                             });
-                            $calendar.fullCalendar('renderEvent', eventData, true); // stick? = true
+                            
+                            // if create false dont create the event a throws a message to ask for new time.
+                            if(create){
+                                $calendar.fullCalendar('renderEvent', eventData, true); // stick? = true
+                            }
+
+                            else{
+                                alert('Por favor seleccione un nuevo horario');
+                            }
+                            
                         }
+                        // Clear selection
                         $calendar.fullCalendar('unselect');   
                     }
                     
